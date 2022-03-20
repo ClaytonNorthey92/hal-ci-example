b7c506fac04e807cf7ff8efea4fc48589cec8897
ClaytonNorthey92 <clayton.northey@gmail.com>
2022-03-20 10:25:56 -0700

    squash

--- evaluate_tests.py
+++ evaluate_tests.py
@@ -4,8 +4,13 @@ import sys
 
 if __name__ == '__main__':
     try:
+        # run the actual tests, this assumes the tests will run in less than 5 seconds.
+        # as the number of tests grows, the timeout will need to be increased
+        # this is done this way because the tests run in a VM, so they are hard to get the
+        # exit code from, so we timeout and read the logs to find the results
         subprocess.run(['make', 'run-arm', '>', 'test_log.txt'], timeout=5)
     except subprocess.TimeoutExpired as e:
+        # we are expecting the tests to timeout, ignore this error and get the status from the logs
         pass
     with open('test_log.txt') as test_log:
         logs = test_log.read();
